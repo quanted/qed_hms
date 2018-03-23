7f7294003a32e8612162dcaf65b133d19baae9b1
Bavant <bavant84@gmail.com>
2018-03-23 15:53:50 -0400

    added image for hydrodynamics page

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 #regular expressions
 # the r in r'^cts/index.html$' indicates that what is inside the quotes is a regular expression
 # the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
-# the $ in r'^cts/index.html$' indicates that we are looking to extend end the mathing part exactly here
+# the $ in r'^cts/index.html$' indicates that we are looking to extend end the matching part exactly here
 
 print('qed.urls')
 #appends to the list of url patterns to check against
